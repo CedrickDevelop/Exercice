41f08ea7a114ccfa3a81a690aee4367fb16a819d
Cedrick <pommier.cedrick@gmail.com>
2021-12-06 14:08:00 +0100

    creation de formulaire de base ajout

--- src/AppBundle/Controller/DepartementController.php
+++ src/AppBundle/Controller/DepartementController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Departement;
+use AppBundle\Form\DepartementType;
 
 /**
  * @Route("/Departement")
@@ -13,47 +14,59 @@ use AppBundle\Entity\Departement;
 class DepartementController extends Controller
 {
     /**
-     * @Route("/Ajout", name="AppDepartement)
+     * @Route("/Ajout", name="Departement")
      */
     public function AjoutAction(Request $request)
     {
+        // Creation du formulaire généré et recupertaion des entrées en fonction de l'entité pour le persister directement
         $dep = new Departement();
+        $formDep = $this->createForm(DepartementType::class, $dep);
+        $formDep->handleRequest($request);
 
-        $formDep = $this->createForm(DepartementType::class);
+        $cnx = $this->getDoctrine()->getManager();
+
+        if ($formDep->isSubmitted())
+        {
+            $cnx->persist($dep);
+            $cnx->flush();
+
+            return $this->redirectToRoute('Afficher');
+
+        }
 
         return $this->render('@App/Departement/ajout.html.twig', [
-
+            'form'  =>  $formDep->createView()
         ]);
     }
 
     /**
-     * @Route("/Show")
+     * @Route("/Show", name="Afficher")
      */
     public function ShowAction()
     {
-        return $this->render('AppBundle:Departement:show.html.twig', array(
-            // ...
-        ));
+        return $this->render('@App/Departement/show.html.twig', [
+            
+        ]);
     }
 
     /**
-     * @Route("/Delete")
+     * @Route("/Delete", name="Delete")
      */
     public function DeleteAction()
     {
-        return $this->render('AppBundle:Departement:delete.html.twig', array(
-            // ...
-        ));
+        return $this->render('@App/Departement/delete.html.twig', [
+            
+        ]);
     }
 
     /**
-     * @Route("/Update")
+     * @Route("/Update", name="Update")
      */
     public function UpdateAction()
     {
-        return $this->render('AppBundle:Departement:update.html.twig', array(
-            // ...
-        ));
+        return $this->render('@App/Departement/update.html.twig', [
+            
+        ]);
     }
 
 }
--- src/AppBundle/Form/DepartementType.php
+++ src/AppBundle/Form/DepartementType.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,27 @@ class DepartementType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('domaine');
+        $builder
+        ->add('name', TextType::class, [
+            'attr'  => [
+                'class' => 'form-control'
+            ]
+        ])
+        ->add('domaine', ChoiceType::class, [
+            'attr'  => [
+                'class' => 'form-control'
+            ],
+            'choices'   =>[
+                'choisir un domaine'    => null,
+                'informatique'          => 'informatique',
+                'gestion'               => 'gestion',
+                'mathematique'          => 'mathematique'
+            ],
+            // 'expanded'  =>true       // Permet de transformer en bouton radio
+        ])
+        ->add('submit', SubmitType::class, [
+
+        ]);
     }/**
      * {@inheritdoc}
      */
